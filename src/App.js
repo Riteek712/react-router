cbe9a870fd423390fb6632b2e7c7936f2ce241a0
Riteek Rakesh <riteek7.12@gmail.com>
2023-07-05 11:26:25 +0530

    Creating proper routes to every component.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import Header from './Header'
-import Nav from './Nav'
-import Footer from './Footer'
-import Home from './Home'
-import NewPost from './NewPost'
-import PostPage from './PostPage'
-import  About from './About'
-import Missing from './Missing'
-import { Route, Routes, useNavigate } from 'react-router-dom'
-import { useState, useEffect } from 'react'
- 
+import Header from "./Header";
+import Nav from "./Nav";
+import Footer from "./Footer";
+import Home from "./Home";
+import NewPost from "./NewPost";
+import PostPage from "./PostPage";
+import About from "./About";
+import Missing from "./Missing";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Nav/>
-      <Home/>
-      <NewPost/>
-      <PostPage/>
-      <About/>
-      <Missing/>
-      <Footer/>
-
+      <Header />
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/NewPost" element={<NewPost />} />
+        <Route path="/Missing" element={<Missing />} />
+        <Route path="/PostPage" element={<PostPage />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,26 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import './nav.css'
 const Nav = () => {
   return (
     <nav>
-        <h1>
-            Nav
-        </h1>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/About'>About</Link>
+          </li>
+          <li>
+            <Link to='/NewPost'>New Post</Link>
+          </li>
+          <li>
+            <Link to='/Missing'>Missing</Link>
+          </li>
+          <li>
+            <Link to='/PostPage'>Post Page</Link>
+          </li>
+        </ul>
     </nav>
   )
 }
